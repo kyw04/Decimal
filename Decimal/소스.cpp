ced41c7d2d23bdbcc3c1b14bdf0158ca60fe778b
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-04-12 20:11:42 +0900

    소수 찾기 개선

--- Decimal/소스.cpp
+++ Decimal/소스.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 bool is_prime(int n)
 {
-	for (int i = 2; i < n; i++)
-	{
+	int sqrn = (int)sqrt(n);
+	for (int i = 2; i < sqrn; i++)
 		if (n % i == 0)
 			return false;
-	}
+
 	return true;
 }
 int main()
