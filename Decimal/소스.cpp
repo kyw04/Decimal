1b0559bb5e659de003d6bb9f4282f612bcad5320
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-04-12 20:02:23 +0900

    초기 커밋.

---
+++ Decimal/소스.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool is_prime(int n)
+{
+	for (int i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	
+	cout << is_prime(n);
+	return 0;
+}
